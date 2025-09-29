26d9e4914f9bea3a3dc21f2c8d145314cd8bb0c0
hmontes <hmontes.dev@gmail.com>
2025-09-29 11:03:58 +0200

    Update

--- app/src/main/java/com/example/tema3componentes/MainActivity.java
+++ app/src/main/java/com/example/tema3componentes/MainActivity.java
@@ -1,6 +1,15 @@
 package com.example.tema3componentes;
 
+import android.app.ActionBar;
+import android.icu.text.ListFormatter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +17,50 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.Console;
+
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    ToggleButton tb;
+    CheckBox cb;
+    SeekBar sb;
+    TextView txv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //Incializo variables
+        btn = findViewById(R.id.button);
+        tb = findViewById(R.id.toggleButton);
+        cb = findViewById(R.id.checkBox3);
+        sb = findViewById(R.id.seekBar);
+        txv = findViewById(R.id.textView4);
+        /*tb.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus){
+                    cb.setActivated(true);
+                }
+                cb.setActivated(false);
+            }
+        });*/
+
+        tb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cb.setChecked(tb.isChecked());
+            }
+        });
+
+        sb.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                txv.setText(v.getScrollX());
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
