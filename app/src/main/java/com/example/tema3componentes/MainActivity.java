28b09e2c34fc4fb74b665a6c8c7afa75acbd97ec
hmontes <hmontes.dev@gmail.com>
2025-10-01 17:19:22 +0200

    Update

--- app/src/main/java/com/example/tema3componentes/MainActivity.java
+++ app/src/main/java/com/example/tema3componentes/MainActivity.java
@@ -1,66 +1,84 @@
 package com.example.tema3componentes;
 
-import android.app.ActionBar;
-import android.icu.text.ListFormatter;
+import android.hardware.biometrics.PromptContentItemPlainText;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.Console;
-
 public class MainActivity extends AppCompatActivity {
-    Button btn;
+
+    EditText ett;
+    Button btt;
+    Button btt2;
+    ImageButton ibtt;
     ToggleButton tb;
-    CheckBox cb;
+    CheckBox ch1;
+    CheckBox ch2;
+    CheckBox ch3;
     SeekBar sb;
-    TextView txv;
+    RatingBar rt;
+    TextView tv;
+    TextView tv2;
+    Switch sw;
 
     RadioButton rb1;
     RadioButton rb2;
 
-    Switch sw;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        //Incializo variables
-        btn = findViewById(R.id.button);
-        tb = findViewById(R.id.toggleButton);
-        cb = findViewById(R.id.checkBox3);
-        sb = findViewById(R.id.seekBar);
-        txv = findViewById(R.id.textView4);
-        sw = findViewById(R.id.switch1);
-        rb1 = findViewById(R.id.radioButton);
-        rb2 = findViewById(R.id.radioButton2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tb=findViewById(R.id.toggleButton);
+        ch1=findViewById(R.id.checkBox);
+        ch2=findViewById(R.id.checkBox2);
+        ch3=findViewById(R.id.checkBox3);
+        sb=findViewById(R.id.seekBar);
+        tv=findViewById(R.id.textView4);
+        tv2=findViewById(R.id.textView2);
+        sw=findViewById(R.id.switch1);
+        btt=findViewById(R.id.button);
+        btt2=findViewById(R.id.button2);
+        rt=findViewById(R.id.ratingBar);
+        ett=findViewById(R.id.editTextText);
+        ibtt=findViewById(R.id.imageButton);
+        rb1=findViewById(R.id.radioButton);
+        rb2=findViewById(R.id.radioButton2);
 
         tb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cb.setChecked(tb.isChecked());
+
+                ch1.setChecked(tb.isChecked());
+                ch3.setEnabled(tb.isChecked());
             }
         });
-
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                txv.setText(sb.getProgress() + "");
+                tv.setText(sb.getProgress()+"");
             }
 
             @Override
@@ -74,48 +92,71 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        sw.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    sw.setText("Activado");
-                } else {
-                    sw.setText("Desactivado");
+            public void onClick(View v) {
+                if (sw.isChecked()){
+                    sw.setText("activo");
+                }else{
+                    sw.setText("desactivo");
                 }
             }
         });
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tb.setChecked(false);
-                rb1.setChecked(false);
-                rb2.setChecked(false);
+                    tb.setChecked(false);
+                    ch1.setChecked(false);
+                    ch2.setChecked(false);
+                    ch3.setChecked(false);
+                    rb1.setChecked(false);
+                    rb2.setChecked(false);
+                if (sb.getProgress()!=0){
+                    sb.setProgress(0);
+                }
+                if(rt.getProgress()!=0){
+                    rt.setProgress(0);
+                }
+                if(!ett.getText().equals("")){
+                    ett.setText("");
+                }
+                btt2.setText("Button");
             }
         });
 
-         /*Al pulsar el botón superior el valor se
-        reinicien los valores de los elementos: el
-        toogle button, los checkboxes, los radio
-        button y el switch pasen a estar no
-        marcados.
+        ibtt.setOnClickListener(new View.OnClickListener() {
+            int contador=0;
 
-        El seekbar pase a tener un
-        valor de 0. El rating bar pase a tener 0
-        estrellas seleccionadas y se borre el
-        texto que pueda tener el editText.
-·       Al pulsar el botón con imagen el texto
-        adyacente se sustituye por un contador
-        cuyo valor se incrementa cada vez que
-        se pulsa el botón.
-·       Si el segundo checkbox está marcado al
-        pulsar el botón el valor del contador decrece.
-·       Al pulsar un RadioButton se muestre un Toast informando del
-        RadioButton pulsado*/
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            @Override
+            public void onClick(View v) {
+
+
+                if (ch2.isChecked()){
+                    contador--;
+                    btt2.setText(contador+"");
+                }else{
+                    contador++;
+                    btt2.setText(contador+"");
+                }
+            }
+
         });
+
+        rb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "radioButton 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rb2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "radioButton 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
